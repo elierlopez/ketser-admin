b96bc32c649ac71bffbc895466f3c6caaee6daea
ketser <ketser@ketsers-MacBook-Pro.local>
2019-01-13 14:52:56 -0600

    arregle un un error estupido que hice de usar react-thunk en lugar de redux-thunk :(

---
+++ src/notas.js
@@ -0,0 +1,14 @@
+/*
+domingo 14 de enero '19
+Un error muy estupido de los que cometi
+en el store.js
+estaba haciendo este import:
+    import thunk from 'react-thunk' q
+    que me installa alguna cosa que no se para que sierve pero no es para las funciones async
+    el que debo installar es
+    import thunk from 'redux-thunk'
+
+    Lo que sigue es llamar al url de ketser y ver si se trae los datos
+
+    */
+
--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux'
-import thunk from 'react-thunk'
+import thunk from 'redux-thunk'
 // import reducers from './Reducers/'
 import * as actionType from './Actions/types'
 
